8491bd77e62f00abd7363f9ac63c0cde6d9257c2
Rodolfo Bonilla <bonilla.rodolfo34@gmail.com>
2021-04-30 13:42:57 -0600

    funcional la parte de pasar de float a binario

--- convert_audio_to_txt_2.0.py
+++ convert_audio_to_txt_2.0.py
@@ -3,68 +3,77 @@
 #pip install sounddevice
 import soundfile as sf
 import sounddevice as sd
-# Python program to convert float
-# decimal to binary number
-  
-# Function returns octal representation
-def float_bin(number, places = 3):
-    #print("BBB")
-    print(number)
-    number=float(number)
-    na=int(number)
-    #print(na)
+#Decimal floating point number to binary
+def dectbin(num):
+    # Determine whether it is a floating point number
+    na=int(num)
     if ((na>=1) or (na<=-1)):
         return '00000000'
     signo=''
-    if (number<0):
+    if (num<0):
         signo='1'
     else:
         signo='0'
-    number=abs(number)
-    number=str(number)
-    #print("---")
-    #print(number)
-    #print(type(number))
+    num=abs(num)
     
-    # split() seperates whole number and decimal 
-    # part and stores it in two seperate variables
-    whole, dec = str(number).split(".")
-  
-    # Convert both whole number and decimal  
-    # part from string type to integer type
-    whole = int(whole)
-    dec = int (dec)
-  
-    # Convert the whole number part to it's
-    # respective binary form and remove the
-    # "0b" from it.
-    res = bin(whole).lstrip("0b") #+ "."
-  
-    # Iterate the number of times, we want
-    # the number of decimal places to be
-    for x in range(places):
-  
-        # Multiply the decimal value by 2 
-        # and seperate the whole number part
-        # and decimal part
-        whole, dec = str((decimal_converter(dec)) * 2).split(".")
-  
-        # Convert the decimal part
-        # to integer again
-        dec = int(dec)
-  
-        # Keep adding the integer parts 
-        # receive to the result variable
-        res += whole
-  
-    return signo+res
-  
-# Function converts the value passed as
-# parameter to it's decimal representation
-def decimal_converter(num): 
-    while num > 1:
-        num /= 10
-    return num
+    if num == int(num):
+        # If it is an integer
+        integer = '{:b}'.format(int(num))
+        return integer
+    else:
+        # If it is a floating point number
+        # Take the integer part
+        integer_part = int(num)
+        # Take the decimal part
+        decimal_part = num - integer_part
+        # Integer part conversion
+        integercom = '{:b}'.format(integer_part)  #{:b}.foemat where b is binary
+        # Decimal conversion
+        tem = decimal_part
+        tmpflo = []
+        # for i in range(accuracy):
+        A = True
+        while A:
+            tem *= 2
+            tmpflo += str(int(tem))  #If the integer part is 0, the binary part is 0, if the integer part is 1, the binary part is 1 #Put 1 or 0 into the list
+            if tem > 1 :   #If multiplied by 2 is a decimal greater than 1, then the integer part must be subtracted
+                tem -= int(tem)
+            elif tem < 1:  #If it is still a decimal number less than 1, after multiplying by 2, continue to use this number to multiply by 2 to convert the base
+                pass
+            else:    #When multiplied by 2 it happens to be 1, then the base conversion stops
+                break
+        flocom = tmpflo
+        #print("signo: "+ signo + " entera: " +integercom  + " coma: " + ''.join(flocom))
+        return signo + integercom  + ''.join(flocom)
+def aux(num):
+    n=dectbin(num)
+    largo=len(n)
+    l=[]
+    if largo<8:
+        for i in range(largo, 8):
+            #print(i)
+            l.append('0')
+        p=listToString(l)
+        nz=''.join((n,p))
+        return nz
+    elif largo>8:
+       h=Convert(n)
+       r=h[0:8]
+       p=listToString(r)
+       nz=''.join(p)
+       return nz
+    else:
+        return n
+
+def listToString(s): 
+    str1 = "" 
+    return (str1.join(s))
+
+def Convert(string):
+    list1=[]
+    list1[:0]=string
+    return list1
+
 
 #se carga el nombre del archivo, CAMBIAR EL NOMBRE DEL ARCHIVO
 nombre_archivo='emergency001.wav'
@@ -88,7 +97,7 @@ if (data.ndim > 1):
     #se escribe el canal mono en una lista
     with open('audio_original.txt', 'w') as archivo:
         for item in mono:
-            archivo.write(float_bin(float(round(item,4)), places=6)+"\n")#"%.6f\n" % item)
+            archivo.write(aux(item)+"\n")#"%.6f\n" % item)
 #para MONO
 else:
     with open('audio_original.txt', 'w') as archivo:
@@ -96,4 +105,4 @@ else:
             #print(type(item))
             #print(type(item.item()))
             #print(item)
-            archivo.write(float_bin(float(round(item,4)), places = 6)+"\n")#"%.6f\n" % item)
+            archivo.write(aux(item)+"\n")#"%.6f\n" % item)
